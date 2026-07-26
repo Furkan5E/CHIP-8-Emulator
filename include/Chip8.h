768857907d9d2c6463756cddf85d57b150664c59
Furkan5E <furkankeles10@hotmail.com>
2026-07-26 17:52:46 +0100

    feat: implemented CHIP-8 hardware initialisation and fontset loading

--- include/Chip8.h
+++ include/Chip8.h
@@ -1,7 +1,25 @@
 #pragma once
+#include <cstdint>
 
 class Chip8 {
 public:
     Chip8();
     void cycle();
+
+    //memory and registers
+    uint8_t memory[4096];
+    uint8_t registers[16];
+    uint16_t index;
+    uint16_t pc;
+
+    //stack and timers
+    uint16_t stack[16];
+    uint8_t sp;
+    uint8_t delay_timer;
+    uint8_t sound_timer;
+
+    // I/O
+    uint8_t keypad[16]; //hex keypad
+    uint32_t display[64 * 32]; //64x32 display pixels
+    uint16_t opcode; //current executing opcode
 };
