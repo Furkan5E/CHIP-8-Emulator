b8e006c8bff5f0a099e6d6bde79848390060509d
Furkan5E <furkankeles10@hotmail.com>
2026-08-13 15:45:26 +0100

    feat: created SDL2 window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,63 @@
+#define SDL_MAIN_HANDLED
 #include "Chip8.h"
+#include <SDL.h>
+#include <iostream>
 
 int main(int argc, char* argv[]) {
-    Chip8 emulator;
+    SDL_SetMainReady();
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << "\n";
+        return -1;
+    }
+    //create window
+    const int SCALE = 10;
+    SDL_Window* window = SDL_CreateWindow(
+        "CHIP-8 Emulator", 
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
+        64 * SCALE, 32 * SCALE, 
+        SDL_WINDOW_SHOWN
+    );
+    if (!window) {
+        std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << "\n";
+        SDL_Quit();
+        return -1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    // create texture
+    SDL_Texture* texture = SDL_CreateTexture(
+        renderer, 
+        SDL_PIXELFORMAT_ARGB8888, 
+        SDL_TEXTUREACCESS_STREAMING, 
+        64, 32
+    );
+
+    Chip8 cpu;
+    //cpu.loadROM("roms/test_opcode.ch8");
+
+    //game loop
+    bool quit = false;
+    SDL_Event e;
+    while (!quit) {
+        // Handle input events
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+
+        //draw CPU display array to screen
+        SDL_UpdateTexture(texture, nullptr, cpu.display, sizeof(cpu.display[0]) * 64);
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+        SDL_RenderPresent(renderer);
+    }
+
+    //clean up
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
